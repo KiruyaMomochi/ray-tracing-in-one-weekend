6ed4aa44bda62face017830d40e18fd1922fe92e
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2022-11-19 02:47:32 +0800

    refactor: RayTracer

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,103 @@ mod world;
 
 pub use camera::Camera;
 pub use hit::Hit;
+use indicatif::ProgressBar;
 pub use ray::Ray;
 pub use sphere::Sphere;
 pub use vec3::{Color, Point3, Vec3};
 pub use world::World;
+
+use std::{error::Error, io::Write};
+
+pub struct RayTracer {
+    aspect_ratio: f64,
+    pub world: World,
+    pub camera: Camera,
+}
+
+const COLOR_MAX: u8 = 255;
+
+impl RayTracer {
+    pub fn new(world: World, camera: Camera) -> Self {
+        Self {
+            aspect_ratio: camera.viewport_width / camera.viewport_height,
+            world,
+            camera,
+        }
+    }
+
+    pub fn trace_in<T: Write>(
+        &self,
+        buffer: &mut T,
+        image_height: u64,
+        t_min: f64,
+        t_max: f64,
+    ) -> Result<(), Box<dyn Error>> {
+        let camera = &self.camera;
+        let world = &self.world;
+
+        let image_width: u64 = (self.aspect_ratio * image_height as f64) as u64;
+
+        writeln!(buffer, "P3")?;
+        writeln!(buffer, "{} {}", image_width, image_height)?;
+        writeln!(buffer, "{}", COLOR_MAX)?;
+
+        let bar = ProgressBar::new(image_height);
+
+        for j in (0..image_height).rev() {
+            bar.set_length(j);
+            for i in 0..image_width {
+                let (i, j) = (i as f64, j as f64);
+                let (width, height) = (image_width as f64, image_height as f64);
+
+                // u: left 0.0 -> 1.0 right
+                // v: botm 0.0 -> 1.0 up
+                let (u, v) = (i / (width - 1.0), j / (height - 1.0));
+
+                let direction =
+                    camera.lower_left_corner + u * camera.horizontal + v * camera.vertical
+                        - camera.origin;
+                let ray = Ray::new(camera.origin, direction);
+
+                let pixel = ray_color(&ray, world, t_min, t_max);
+                writeln!(buffer, "{}", pixel.format_color())?;
+            }
+        }
+
+        bar.finish();
+
+        Ok(())
+    }
+
+    pub fn trace<T: Write>(&self, buffer: &mut T, image_height: u64) -> Result<(), Box<dyn Error>> {
+        self.trace_in(buffer, image_height, 0.0, f64::INFINITY)
+    }
+}
+
+/// Returns the color of the ray-tracing
+///
+/// When our ray hits a sphere, the color is red.
+///
+/// Background color is a simple gradient, which
+/// linearly blends white and blue depending on the height of the y coordinate.
+pub fn ray_color<T: Hit>(ray: &Ray, hittable: &T, t_min: f64, t_max: f64) -> Color {
+    if let Some(hit) = ray.hit(hittable, t_min, t_max) {
+        // Obtain the unit normal vector: -1 <= . <= 1
+        let normal = hit.normal_outward.normalized();
+        // For color, scale to 0 <= . <= 1
+        let color = 0.5 * (normal + 1.0);
+
+        return color;
+    }
+
+    // Scale the ray direction to unit length -1 <= direction.y <= 1
+    let direction = ray.direction().normalized();
+
+    // Scale direction into 0 <= t <= 1
+    let t = 0.5 * (direction.y() + 1.0);
+
+    // linear blend / linear interpolation / lerp
+    // blended = (1 - t) * start + t * end
+    let blue = Color::new(0.5, 0.7, 1.0);
+    (1.0 - t) * Color::white() + t * blue
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
-use indicatif::ProgressBar;
-use ray_tracing_in_one_weekend::{Color, Point3, Ray, Sphere, Camera, World, Hit};
-use std::{
-    fs,
-    io::{BufWriter, Write},
-};
+use ray_tracing_in_one_weekend::{Camera, Point3, RayTracer, Sphere, World};
+use std::{error::Error, fs, io::BufWriter};
 
-/// Returns the color of the ray-tracing
-///
-/// When our ray hits a sphere, the color is red.
-///
-/// Background color is a simple gradient, which
-/// linearly blends white and blue depending on the height of the y coordinate.
-pub fn ray_color<T: Hit>(ray: &Ray, hittable: &T) -> Color {
-    if let Some(hit) = ray.hit(hittable, 0.0, f64::INFINITY) {
-        // Obtain the unit normal vector: -1 <= . <= 1
-        let normal = hit.normal_outward.normalized();
-        // For color, scale to 0 <= . <= 1
-        let color = 0.5 * (normal + 1.0);
-
-        return color;
-    }
-
-    // Scale the ray direction to unit length -1 <= direction.y <= 1
-    let direction = ray.direction().normalized();
-
-    // Scale direction into 0 <= t <= 1
-    let t = 0.5 * (direction.y() + 1.0);
-
-    // linear blend / linear interpolation / lerp
-    // blended = (1 - t) * start + t * end
-    let blue = Color::new(0.5, 0.7, 1.0);
-    (1.0 - t) * Color::white() + t * blue
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     // Image
     // Use 16:9 aspect ratio
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_HEIGHT: u64 = 255;
-    const IMAGE_WIDTH: u64 = (ASPECT_RATIO * IMAGE_HEIGHT as f64) as u64;
-    const COLOR_MAX: u8 = 255;
 
     // World
     let mut world = World::new();
@@ -52,32 +18,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut file = BufWriter::new(fs::File::create("image.ppm")?);
 
-    writeln!(file, "P3")?;
-    writeln!(file, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT)?;
-    writeln!(file, "{}", COLOR_MAX)?;
-
-    let bar = ProgressBar::new(IMAGE_HEIGHT);
-
-    for j in (0..IMAGE_HEIGHT).rev() {
-        bar.set_length(j);
-        for i in 0..IMAGE_WIDTH {
-            let (i, j) = (i as f64, j as f64);
-            let (width, height) = (IMAGE_WIDTH as f64, IMAGE_HEIGHT as f64);
-
-            // u: left 0.0 -> 1.0 right
-            // v: botm 0.0 -> 1.0 up
-            let (u, v) = (i / (width - 1.0), j / (height - 1.0));
-
-            let direction = camera.lower_left_corner + u * camera.horizontal + v * camera.vertical
-                - camera.origin;
-            let ray = Ray::new(camera.origin, direction);
-
-            let pixel = ray_color(&ray, &world);
-            writeln!(file, "{}", pixel.format_color())?;
-        }
-    }
-
-    bar.finish();
+    let tracer = RayTracer::new(world, camera);
+    tracer.trace(&mut file, IMAGE_HEIGHT)?;
 
     Ok(())
 }
