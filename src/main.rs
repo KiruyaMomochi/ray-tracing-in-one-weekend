3c2b116a4682d3c0c67e97328f8a7b053311c616
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2022-11-18 21:30:17 +0800

    5. Rays, a Simple Camera, and Background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
+use indicatif::ProgressBar;
+use std::{
+    fs,
+    io::{BufWriter, Write},
+};
+use vec3::{Point3, Vec3};
+
+mod ray;
+mod vec3;
+
+use crate::{ray::Ray, vec3::Color};
+
+/// Simple camera
+#[derive(Debug, Clone)]
+struct Camera {
+    // Virtual viewport to pass scene rays
+    pub viewport_height: f64,
+    pub viewport_width: f64,
+    /// Distance between projection plane and projection point
+    pub focal_length: f64,
+    pub origin: Point3,
+    /// x-asis
+    pub horizontal: Vec3<f64>,
+    /// y-axis
+    pub vertical: Vec3<f64>,
+    pub lower_left_corner: Point3,
+}
+
+impl Camera {
+    pub fn new(viewport_height: f64, aspect_ratio: f64, focal_length: f64) -> Self {
+        let origin = Point3::zero();
+        let horizontal = Vec3::new(viewport_height, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Self {
+            viewport_height,
+            viewport_width: viewport_height * aspect_ratio,
+            focal_length,
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner,
+        }
+    }
+}
+
+/// Returns the color of the background (a simple gradient)
+///
+/// Linearly blends white and blue depending on the height of the y coordinate.
+fn ray_color(ray: &Ray) -> Color {
+    // Scale the ray direction to unit length -1 <= direction.y <= 1
+    let direction = ray.direction().normalized();
+
+    // Scale direction into 0 <= t <= 1
+    let t = 0.5 * (direction.y() + 1.0);
+    
+    // linear blend / linear interpolation / lerp
+    // blended = (1 - t) * start + t * end
+    let blue = Color::new(0.5, 0.7, 1.0);
+    (1.0 - t) * Color::white() + t * blue
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Image
+
+    // Use 16:9 aspect ratio
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u64 = 255;
+    const IMAGE_HEIGHT: u64 = 255;
+
+    const COLOR_MAX: u8 = 255;
+
+    // Camera (-1 to 1, -1 to 1, -1 to 0)
+    let camera = Camera::new(2.0, ASPECT_RATIO, 1.0);
+
+    let mut file = BufWriter::new(fs::File::create("image.ppm")?);
+
+    writeln!(file, "P3")?;
+    writeln!(file, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+    writeln!(file, "{}", COLOR_MAX)?;
+
+    let bar = ProgressBar::new(IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        bar.set_length(j);
+        for i in 0..IMAGE_WIDTH {
+            let (i, j) = (i as f64, j as f64);
+            let (width, height) = (IMAGE_WIDTH as f64, IMAGE_HEIGHT as f64);
+
+            // u: 0.0 -> 1.0
+            // v: 0.0 ^ 1.0
+            let (u, v) = (i / (width - 1.0), j / (height - 1.0));
+
+            let direction = camera.lower_left_corner + u * camera.horizontal + v * camera.vertical
+                - camera.origin;
+            let ray = Ray::new(camera.origin, direction);
+
+            let pixel = ray_color(&ray);
+            writeln!(file, "{}", pixel.format_color())?;
+        }
+    }
+
+    bar.finish();
+
     Ok(())
 }
---
+++ src/vec3.rs
@@ -0,0 +1,232 @@
+use std::{
+    fmt::Display,
+    ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
+};
+
+const COLOR_MAX: f64 = 255.0;
+
+#[derive(Clone, Debug, Copy)]
+pub struct Vec3<T: Copy>([T; 3]);
+
+pub type Point3 = Vec3<f64>;
+pub type Color = Vec3<f64>;
+
+impl<T: Copy> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Self {
+        Self([x, y, z])
+    }
+
+    pub fn from_array(arr: [T; 3]) -> Self {
+        Self(arr)
+    }
+
+    pub fn x(&self) -> T {
+        self[0]
+    }
+
+    pub fn y(&self) -> T {
+        self[1]
+    }
+
+    pub fn z(&self) -> T {
+        self[2]
+    }
+
+    pub fn r(&self) -> T {
+        self[0]
+    }
+
+    pub fn g(&self) -> T {
+        self[1]
+    }
+
+    pub fn b(&self) -> T {
+        self[2]
+    }
+}
+
+impl<T: Copy + Default> Vec3<T> {
+    pub fn zero() -> Self {
+        Self([T::default(); 3])
+    }
+}
+
+impl Vec3<f64> {
+    pub fn unit() -> Self {
+        Self([1.0, 1.0, 1.0])
+    }
+}
+
+impl<T> Vec3<T>
+where
+    T: Copy + Mul<Output = T> + std::ops::Add<Output = T> + std::ops::Sub<Output = T>,
+{
+    pub fn dot(&self, other: &Self) -> T {
+        self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Self::new(
+            self[1] * other[2] - self[2] * other[1],
+            self[2] * other[0] - self[0] * other[2],
+            self[0] * other[1] - self[1] * other[0],
+        )
+    }
+
+    pub fn len_squared(&self) -> T {
+        self.dot(self)
+    }
+}
+
+impl Vec3<f64> {
+    pub fn len(&self) -> f64 {
+        self.len_squared().sqrt()
+    }
+
+    pub fn normalized(self) -> Self {
+        self / self.len()
+    }
+}
+
+impl<T: Copy> Index<usize> for Vec3<T> {
+    type Output = T;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.0[i]
+    }
+}
+
+impl<T: Copy> IndexMut<usize> for Vec3<T> {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        &mut self.0[i]
+    }
+}
+
+impl<T: Copy> Add for Vec3<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2])
+    }
+}
+
+impl<T: Copy> AddAssign for Vec3<T>
+where
+    T: AddAssign,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        self[0] += rhs[0];
+        self[1] += rhs[1];
+        self[2] += rhs[2];
+    }
+}
+
+impl<T: Copy> Sub for Vec3<T>
+where
+    T: Sub<Output = T>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self[0] - rhs[0], self[1] - rhs[1], self[2] - rhs[2])
+    }
+}
+
+impl<T: Copy> SubAssign for Vec3<T>
+where
+    T: SubAssign,
+{
+    fn sub_assign(&mut self, rhs: Self) {
+        self[0] -= rhs[0];
+        self[1] -= rhs[1];
+        self[2] -= rhs[2];
+    }
+}
+
+impl<T: Copy> Mul<T> for Vec3<T>
+where
+    T: Mul<Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::new(self[0] * rhs, self[1] * rhs, self[2] * rhs)
+    }
+}
+
+impl Mul<Vec3<f64>> for f64
+{
+    type Output = Vec3<f64>;
+
+    fn mul(self, rhs: Vec3<f64>) -> Self::Output {
+        Vec3::new(self * rhs[0], self * rhs[1], self * rhs[2])
+    }
+}
+
+impl<T: Copy> MulAssign<T> for Vec3<T>
+where
+    T: MulAssign,
+{
+    fn mul_assign(&mut self, rhs: T) {
+        self[0] *= rhs;
+        self[1] *= rhs;
+        self[2] *= rhs;
+    }
+}
+
+impl<T: Copy> Div<T> for Vec3<T>
+where
+    T: Div<Output = T>,
+{
+    type Output = Self;
+
+    fn div(self, rhs: T) -> Self::Output {
+        Self::new(self[0] / rhs, self[1] / rhs, self[2] / rhs)
+    }
+}
+
+impl<T: Copy> DivAssign<T> for Vec3<T>
+where
+    T: DivAssign,
+{
+    fn div_assign(&mut self, rhs: T) {
+        self[0] /= rhs;
+        self[1] /= rhs;
+        self[2] /= rhs;
+    }
+}
+
+impl<T: Copy> Mul for Vec3<T>
+where
+    T: Mul<Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::new(self[0] * rhs[0], self[1] * rhs[1], self[2] * rhs[2])
+    }
+}
+
+impl<T: Copy + Display> Display for Vec3<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self[0], self[1], self[2])
+    }
+}
+
+impl Color {
+    pub fn format_color(&self) -> String {
+        format!(
+            "{} {} {}",
+            (COLOR_MAX * self[0]).round() as u64,
+            (COLOR_MAX * self[1]).round() as u64,
+            (COLOR_MAX * self[2]).round() as u64
+        )
+    }
+
+    pub fn white() -> Self {
+        Self::unit()
+    }
+}
