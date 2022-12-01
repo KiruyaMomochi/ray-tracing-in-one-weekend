082328a09fd2d6f1841f915b6e5505ff56b3b5a7
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2022-12-02 06:37:53 +0900

    14.1. A Final Render

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,85 @@
+use rand::Rng;
 use ray_tracing_in_one_weekend::{
     material::{Dielectric, Lambertian, Metal},
     Camera, Color, Point3, RayTracer, Sphere, Vec3, World,
 };
 use std::{error::Error, fs, io::BufWriter, rc::Rc};
 
+fn random_scene() -> World {
+    let mut rng = rand::thread_rng();
+    let mut world = World::new();
+
+    let ground_mat = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    let ground_sphere = Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_mat);
+
+    world.add(ground_sphere);
+
+    for a in -11..=11 {
+        for b in -11..=11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new((a as f64) + rng.gen_range(0.0..0.9),
+                                     0.2,
+                                     (b as f64) + rng.gen_range(0.0..0.9));
+
+            if choose_mat < 0.8 {
+                // Diffuse
+                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
+                let sphere_mat = Rc::new(Lambertian::new(albedo));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.add(sphere);
+            } else if choose_mat < 0.95 {
+                // Metal
+                let albedo = Color::random(0.4..1.0);
+                let fuzz = rng.gen_range(0.0..0.5);
+                let sphere_mat = Rc::new(Metal::new(albedo, fuzz));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.add(sphere);
+            } else {
+                // Glass
+                let sphere_mat = Rc::new(Dielectric::new(1.5));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.add(sphere);
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.5));
+    let mat2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    let mat3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+
+    let sphere1 = Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat1);
+    let sphere2 = Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat2);
+    let sphere3 = Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat3);
+
+    world.add(sphere1);
+    world.add(sphere2);
+    world.add(sphere3);
+
+    world
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Image
     // Use 16:9 aspect ratio
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
     const IMAGE_HEIGHT: u64 = 255;
     const SAMPLES_PER_PIXEL: u64 = 100;
-    const MAX_DEPTH: i64 = 50;
+    const MAX_DEPTH: i64 = 500;
 
     // World
-    let mut world = World::new();
-
-    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = Rc::new(Dielectric::new(1.5));
-    let mat_left_inner = mat_left.clone();
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
-
-    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
-    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
-    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
-    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.45, mat_left_inner);
-    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
-
-    world.add(sphere_ground);
-    world.add(sphere_center);
-    world.add(sphere_left);
-    world.add(sphere_left_inner);
-    world.add(sphere_right);
+    let world = random_scene();
 
     // Camera (-1 to 1, -1 to 1, -1 to 0)
     let camera = Camera::new(
-        Point3::new(3.0, 3.0, 2.0),
-        Point3::new(0.0, 0.0, -1.0),
+        Point3::new(13.0, 2.0, 3.0),
+        Point3::new(0.0, 0.0, 0.0),
         Vec3::new(0.0, 1.0, 0.0),
-        20.0,
+        10.0,
         ASPECT_RATIO,
-        2.0,
+        0.1,
     );
     println!("{}", camera);
 
