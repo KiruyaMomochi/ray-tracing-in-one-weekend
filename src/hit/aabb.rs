369cad67b7557b3c96899a6fe422f27735966fb7
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2022-12-11 08:11:02 +0800

    AABB merge

--- src/hit/aabb.rs
+++ src/hit/aabb.rs
@@ -1,4 +1,4 @@
-use crate::{Point3, Ray};
+use crate::{Point3, Ray, Vec3};
 
 /// Axis aligned bounding box (AABB).
 /// AABBs are used to determine whether two objects are colliding.
@@ -8,7 +8,7 @@ use crate::{Point3, Ray};
 /// then it might be hitting one of the 10 objects. And all we need to know is whether
 /// or not we hit it; we don’t need hit points or normals or any of that stuff that we
 /// need for an object we want to display.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct AABB {
     /// Minimum point of the AABB
     pub min: Point3,
@@ -61,9 +61,18 @@ impl AABB {
         true
     }
 
-    pub fn merge(self, other: Self) -> Self {
+    /// Combines two AABBs into a single AABB that contains both.
+    pub fn merge(&self, other: &Self) -> Self {
         let min = self.min.min(&other.min);
         let max = self.max.max(&other.max);
+
+        Self::new(min, max)
+    }
+
+    /// Includes a point in the AABB.
+    pub fn include(self, point: &Point3) -> Self {
+        let min = self.min.min(point);
+        let max = self.max.max(point);
 
         Self::new(min, max)
     }
@@ -74,5 +83,30 @@ impl AABB {
 
     pub fn max(&self) -> Point3 {
         self.max
+    }
+
+    pub fn move_by(self, offset: Vec3<f64>) -> AABB {
+        Self { min: self.min + offset, max: self.max + offset }
+    }
+
+    /// Returns the point at the corner of the AABB with the given index.
+    /// The index is a bit mask where the first bit is the x coordinate, the second
+    /// bit is the y coordinate, and the third bit is the z coordinate.
+    /// The bit is 0 if the coordinate is the minimum coordinate, and 1 if it is the
+    /// maximum coordinate.
+    pub fn corner(&self, index: usize) -> Point3 {
+        assert!(index < (1 << 3));
+        let x = if index & (1 << 0) == 0 { self.min.x() } else { self.max.x() };
+        let y = if index & (1 << 1) == 0 { self.min.y() } else { self.max.y() };
+        let z = if index & (1 << 2) == 0 { self.min.z() } else { self.max.z() };
+        Point3::new(x, y, z)
+    }
+
+    pub fn iter_corners(&self) -> impl Iterator<Item = Point3> + '_ {
+        (0..(1 << 3)).map(move |i| self.corner(i))
+    }
+
+    pub fn into_iter_corners(self) -> impl Iterator<Item = Point3> {
+        (0..(1 << 3)).map(move |i| self.corner(i))
     }
 }
